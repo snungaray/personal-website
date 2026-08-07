522233db5af005ecb8fc7faa24299aabb5e5dd27
Santiago Nungaray <93005954+snungaray729@users.noreply.github.com>
2026-08-06 17:21:33 -0700

    Updates

--- contact-page/contact.js
+++ contact-page/contact.js
@@ -1,6 +1,5 @@
 const form = document.getElementById('form')
 const fname = document.getElementById('fname')
-const lname = document.getElementById('lname')
 const email = document.getElementById('email')
 const message = document.getElementById('message')
 
